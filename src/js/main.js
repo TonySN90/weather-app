0e65a4a650ac7105d97f0e497a3625ee98b82faf
TonySN90 <tony.heider90@gmail.com>
2023-12-13 12:27:53 +0100

    update styling / header section

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-import "./../scss/style.scss";
+import "./../scss/main.scss";
 
 // const locationInput = document.getElementById("locationInput");
 // const weatherContainer = document.getElementById("weatherContainer");
