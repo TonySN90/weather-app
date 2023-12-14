71b3579d75c644ac201e45f92a46871fa4ab90f3
TonySN90 <tony.heider90@gmail.com>
2023-12-15 00:48:32 +0100

    testing api

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 import "./../scss/main.scss";
 
 const searchBtn = document.querySelector(".current-location__button");
@@ -6,16 +8,31 @@ const state = {
   location: "",
 };
 
-const API_KEY = "3f7d15f8a87ecf63772b7fcd776a2c91";
+// API ---------------------
 
-async function getDataFromAPI() {
+const URL = {
+  currentWeather(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`;
+  },
+  forecast(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
+  },
+  airPollution(lat, lon) {
+    return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+  },
+  reverseGeocoding(lat, lon) {
+    return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
+  },
+  geocoding(query) {
+    return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+  },
+};
+const API_KEY = "3f7d15f8a87ecf63772b7fcd776a2c91";
+const defaultLocation = `#/weather?lat=53.62937&lon=11.41316`;
+
+async function fetchData() {
   try {
-    const response = await fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=${API_KEY}`
-    );
-    // const response = await fetch(
-    //   `http://api.openweathermap.org/geo/1.0/direct?q=${state.location}&limit=5&appid=${API_KEY}`
-    // );
+    const response = await fetch(`${URL}&appid=${API_KEY}`);
     const result = await JSON.parse(response);
     console.log(result);
   } catch (error) {
@@ -23,16 +40,58 @@ async function getDataFromAPI() {
   }
 }
 
-// function clearInputField() {
-//   inputFieldEl.value = "";
-// }
+const weekdayNames = [
+  "Sonntag",
+  "Montag",
+  "Dienstag",
+  "Mittwoch",
+  "Donnerstag",
+  "Freitag",
+  "Samstag",
+];
+
+const monthNames = [
+  "Jan",
+  "Feb",
+  "Mär",
+  "Apr",
+  "Mai",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Okt",
+  "Nov",
+  "Dez",
+];
+
+function getDate(dateUnix, timezone) {
+  const date = new Date((dateUnix + timezone) * 1000);
+  const weekdayName = weekdayNames[date.getUTCDay()];
+  const monthName = monthNames[date.getUTCMonth()];
+  return `${weekdayName} ${date.getUTCDate()}, ${monthName}`;
+}
+
+function getTime(timeUnix, timezone) {
+  const date = new Date((timeUnix + timezone) * 1000);
+  const hours = date.getUTCHours();
+  const minutes = date.getUTCMinutes();
+  const period = hours >= 12 ? "PM" : "AM";
+
+  return `${hours % 12 || 12}: ${minutes} ${period}`;
+}
+
+function mpsToKmh(mps) {
+  const mph = mps * 1000;
+  return mph / 1000;
+}
 
 searchBtn.addEventListener("click", async () => {
   const inputFieldEl = document.querySelector(".search-view--input");
   state.location = inputFieldEl.value;
 
   console.log(state.location);
-  // await getDataFromAPI();
+  // await fetchData();
 
-  //   clearInputField();
+  inputFieldEl.value = "";
 });
