f48675cf39e61ad2168bf12416bca503662a47c8
TonySN90 <tony.heider90@gmail.com>
2023-12-15 10:00:17 +0100

    update media queries

--- src/js/main.js
+++ src/js/main.js
@@ -30,68 +30,69 @@ const URL = {
 const API_KEY = "3f7d15f8a87ecf63772b7fcd776a2c91";
 const defaultLocation = `#/weather?lat=53.62937&lon=11.41316`;
 
-async function fetchData() {
+async function fetchData(url, query) {
   try {
-    const response = await fetch(`${URL}&appid=${API_KEY}`);
-    const result = await JSON.parse(response);
+    const response = await fetch(`${url}&appid=${API_KEY}`);
+    const result = await response.json();
     console.log(result);
+    return result;
   } catch (error) {
     console.error(error);
   }
 }
 
-const weekdayNames = [
-  "Sonntag",
-  "Montag",
-  "Dienstag",
-  "Mittwoch",
-  "Donnerstag",
-  "Freitag",
-  "Samstag",
-];
+// const weekdayNames = [
+//   "Sonntag",
+//   "Montag",
+//   "Dienstag",
+//   "Mittwoch",
+//   "Donnerstag",
+//   "Freitag",
+//   "Samstag",
+// ];
 
-const monthNames = [
-  "Jan",
-  "Feb",
-  "Mär",
-  "Apr",
-  "Mai",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Okt",
-  "Nov",
-  "Dez",
-];
+// const monthNames = [
+//   "Jan",
+//   "Feb",
+//   "Mär",
+//   "Apr",
+//   "Mai",
+//   "Jun",
+//   "Jul",
+//   "Aug",
+//   "Sep",
+//   "Okt",
+//   "Nov",
+//   "Dez",
+// ];
 
-function getDate(dateUnix, timezone) {
-  const date = new Date((dateUnix + timezone) * 1000);
-  const weekdayName = weekdayNames[date.getUTCDay()];
-  const monthName = monthNames[date.getUTCMonth()];
-  return `${weekdayName} ${date.getUTCDate()}, ${monthName}`;
-}
+// function getDate(dateUnix, timezone) {
+//   const date = new Date((dateUnix + timezone) * 1000);
+//   const weekdayName = weekdayNames[date.getUTCDay()];
+//   const monthName = monthNames[date.getUTCMonth()];
+//   return `${weekdayName} ${date.getUTCDate()}, ${monthName}`;
+// }
 
-function getTime(timeUnix, timezone) {
-  const date = new Date((timeUnix + timezone) * 1000);
-  const hours = date.getUTCHours();
-  const minutes = date.getUTCMinutes();
-  const period = hours >= 12 ? "PM" : "AM";
+// function getTime(timeUnix, timezone) {
+//   const date = new Date((timeUnix + timezone) * 1000);
+//   const hours = date.getUTCHours();
+//   const minutes = date.getUTCMinutes();
+//   const period = hours >= 12 ? "PM" : "AM";
 
-  return `${hours % 12 || 12}: ${minutes} ${period}`;
-}
+//   return `${hours % 12 || 12}: ${minutes} ${period}`;
+// }
 
-function mpsToKmh(mps) {
-  const mph = mps * 1000;
-  return mph / 1000;
-}
+// function mpsToKmh(mps) {
+//   const mph = mps * 1000;
+//   return mph / 1000;
+// }
 
 searchBtn.addEventListener("click", async () => {
   const inputFieldEl = document.querySelector(".search-view--input");
   state.location = inputFieldEl.value;
 
   console.log(state.location);
-  // await fetchData();
+  // await fetchData(URL.geocoding(state.location));
 
   inputFieldEl.value = "";
 });
